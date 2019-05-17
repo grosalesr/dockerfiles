1a32cba9759845d7f6094199f0c4db6a2887b9e4
Gerardo Rosales <grosales@f-m.fm>
2019-05-16 20:44:49 -0600

    :pencil: reorder execution arguments

--- wine/podman-wine.sh
+++ wine/podman-wine.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 podman run --rm -it \
-    -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
-    -v /dev/dri:/dev/dri \
-	-v /home/$(id -un)/Downloads:/mnt:rw \
-	--workdir="/home/appuser" \
-    --security-opt=label=type:container_runtime_t \
     -e DISPLAY \
-    localhost/grosalesr/wine:fedora /bin/bash
+    --ipc host \
+    -v /dev/dri:/dev/dri \
+    --workdir="/home/appuser" \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
+    -v /home/$(id -un)/Downloads:/mnt:rw \
+    --security-opt=label=type:container_runtime_t \
+    localhost/grosalesr/wine /bin/bash
